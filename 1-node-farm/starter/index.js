910ef6096ca2fd359b28e83a6dab6826d66291e9
jpared3s <jparedes.wall@gmail.com>
2023-06-07 22:29:14 -0400

    sync and async exercise

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+//blocking (synchronous)
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written')
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
+//non-blocking (asynchronous)
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
 
-fs.writeFileSync('./txt/output.txt', textOut);
-
-console.log('File written')
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
+        console.log('Your file has been written ❤️')
+      })
+    })
+  })
+})
+console.log("Will read file")
