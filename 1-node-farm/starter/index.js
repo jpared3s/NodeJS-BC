c3f9196cdfb6cab1c7284624aef368abc68b2e83
jpared3s <jparedes.wall@gmail.com>
2023-06-10 09:48:41 -0400

    routing exercise

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 // const fs = require('fs');
 const http = require("http");
+// const url = require("url");
 
 /////////////////Files;
 
@@ -27,9 +28,19 @@ const http = require("http");
 ////////////////server
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
+    })
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000')
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
