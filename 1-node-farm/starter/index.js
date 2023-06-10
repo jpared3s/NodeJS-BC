da8829b69b2d88c97e09cc443693d1c39f1cfd00
jpared3s <jparedes.wall@gmail.com>
2023-06-10 09:20:22 -0400

    creating simple web server exercise

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
-const fs = require('fs');
+// const fs = require('fs');
+const http = require("http");
+
+/////////////////Files;
 
 //blocking (synchronous)
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
@@ -7,16 +10,26 @@ const fs = require('fs');
 // console.log('File written')
 
 //non-blocking (asynchronous)
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
-        console.log('Your file has been written ❤️')
-      })
-    })
-  })
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', (err) => {
+//         console.log('Your file has been written ❤️')
+//       })
+//     })
+//   })
+// })
+// console.log("Will read file")
+
+////////////////server
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000')
 })
-console.log("Will read file")
