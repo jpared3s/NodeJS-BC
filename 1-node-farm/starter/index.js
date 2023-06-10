bcad3b880066f7d0472d3d4fb7fa3f29fa58fed1
jpared3s <jparedes.wall@gmail.com>
2023-06-10 10:36:16 -0400

    api exercise

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,6 +1,6 @@
-// const fs = require('fs');
+const fs = require("fs");
 const http = require("http");
-// const url = require("url");
+const url = require("url");
 
 /////////////////Files;
 
@@ -26,6 +26,8 @@ const http = require("http");
 // console.log("Will read file")
 
 ////////////////server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -33,10 +35,13 @@ const server = http.createServer((req, res) => {
     res.end("This is the overview");
   } else if (pathName === "/product") {
     res.end("This is the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
-      'Content-type': 'text/html'
-    })
+      "Content-type": "text/html",
+    });
     res.end("<h1>Page not found</h1>");
   }
 });
